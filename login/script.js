28ea87235bd0cfa48174bf8cf9c6d8c8b0f4dd19
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-12-10 21:46:02 -0800

    More Information

--- login/script.js
+++ login/script.js
@@ -1,18 +1,18 @@
 const containers = document.querySelectorAll('.sub-container');
 const imageContainer = document.getElementById('image-container')
-const images = ['../images/night.jpg', '../images/swamp.jpg'];
+const images = ['../images/night.jpg', '../images/swamp.jpg', '../images/desert.jpg'];
 
 let activeContainer = 0;
 
 function toggleContainer(num){
     containers[activeContainer].classList.remove('active-container');
     containers[activeContainer].classList.add('inactive-container');
-    containers[num].classList.add('active-container');
     containers[num].classList.remove('inactive-container');
+    containers[num].classList.add('active-container');
 
     imageContainer.style.backgroundImage = "url('" + images[num] + "')"
 
     activeContainer = num;
 }
 
-imageContainer.style.backgroundImage = "url('" + images[0] + "')"
+toggleContainer(0);
