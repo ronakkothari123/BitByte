dcfcd38b634e4e46d741e94abf4daa035a39cc4b
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-12-11 17:54:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -38,4 +38,7 @@ function createProduct(color, name, desc, version){
 }
 
 createProduct("blue", "BitFolio", "An Online Social Media dedicated to showcase your personal achievements, advancements, qualifications, etc. The Best Online Hub for employees and employers everywhere!", "BETA")
-createProduct("purple", "BitSports", "Online Fantasy Sports platform for players around the world. Is not constricted to one location, and is free to play! It is The Number One Fantasy Sport application in the world!")
+createProduct("purple", "BitSports", "Online Fantasy Sports platform for players around the world. Is not constricted to one location, and is free to play! It is The Number One Fantasy Sport application in the world!", "ALPHA")
+createProduct("red", "BitMail", "Online Email system designed for efficiency and user-friendliness. This email system utilizes End-To-End encryption and secure connections enabling an integrated security system!", "ALPHA")
+createProduct("yellow", "BitLearn", "A Website dedicated to helping people learn about everyday necessities, whilst making it fun and easy. Our curriculum emphasizes user understanding, and has an online community to help.", "BETA")
+createProduct("green", "BitPlay", "A platform for online games that you can play! As of right now, it is free, but fun! The Games were designed with user-friendliness and customizability in mind. Have Fun gaming!", "ALPHA")
