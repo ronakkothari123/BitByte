e7a576a928c20b1afe071bd7f9375150d6ec20e7
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-12-15 23:22:08 -0800

    Update script.js

--- Sports/Dashboard/script.js
+++ Sports/Dashboard/script.js
@@ -44,6 +44,7 @@ function createFantasyLeague(sportIndex, name, prize, competing, image){
 }
 
 createFantasyLeague(1, "NFL 2022 Fantasy", "1 Ultimate Trophy", 7284854, "https://cdn.vox-cdn.com/thumbor/X0BcMsovx2iiZvMrSc4bgQk5y-g=/0x0:1200x800/1200x800/filters:focal(504x304:696x496)/cdn.vox-cdn.com/uploads/chorus_image/image/70203570/Week12WinnersLosers_AP_Ringer.0.jpeg")
-createFantasyLeague(0, "IPL 2022 Cup", "1 Silver Trophy", 4567837, "https://pbs.twimg.com/media/E9FpDDFXsAAqkbR.jpg")
+createFantasyLeague(0, "IPL 2022 Cup", "1 Silver Trophy", 4567837, "https://wallpaperaccess.com/full/2302746.jpg")
 createFantasyLeague(2, "NBA 2022 Fantasy", "1 Ultimate Trophy", 12, "https://cdn.nba.com/manage/2021/10/3point-lead-graphic.png")
 createFantasyLeague(3, "FIFA 2022 World Cup", "1 Ultimate Trophy", 14843725, "https://static.standard.co.uk/2021/05/21/19/newFile.jpg?width=968&auto=webp&quality=75&crop=968%3A645%2Csmart")
+createFantasyLeague(0, "ICC T20 World Cup", "1 Ultimate Trophy", 982374, "https://pbs.twimg.com/media/E9FpDDFXsAAqkbR.jpg");
