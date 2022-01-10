3ed2f83339031d68d03af976208f8406794d30a8
Advay Patil <advaypatil27@gmail.com>
2022-01-09 20:42:15 -0800

    nothing

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ import draftRouter from "./routers/draft";
 import trophiesRouter from "./routers/trophies";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 dotenv.config();
 
